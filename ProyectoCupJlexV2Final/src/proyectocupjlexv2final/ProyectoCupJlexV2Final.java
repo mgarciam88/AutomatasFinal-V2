89349e4f68866e0f065ba3e7290014ebd5392421
mgarm <mgarm@host.docker.internal>
2020-11-09 18:28:35 -0600

    Proyecto Final

---
+++ ProyectoCupJlexV2Final/src/proyectocupjlexv2final/ProyectoCupJlexV2Final.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectocupjlexv2final;
+
+/**
+ *
+ * @author mgarm
+ */
+public class ProyectoCupJlexV2Final {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Principal llamada = new Principal();
+        llamada.setVisible(true);
+    }
+    
+}
